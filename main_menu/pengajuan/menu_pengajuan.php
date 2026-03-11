897d47adfa105c652e7c639c3a965495caeeb9a6
L-Candra-H <robert.triobule1107@gmail.com>
2026-03-11 20:21:33 +0700

    Update menu_pengajuan.php

--- main_menu/pengajuan/menu_pengajuan.php
+++ main_menu/pengajuan/menu_pengajuan.php
@@ -6,7 +6,15 @@ if(!isset($_SESSION["ses_pengajuan_login"])) {
 }
 
 $nama_user = $_SESSION['nama_pengaju']; // gunakan nama_pengaju sesuai login.php
+
+include_once '../conf/conf.php';
+include_once '../conf/conf_pengajuan.php';
+$conn_pengajuan = bukakoneksi_pengajuan();
+
+// ambil data instansi
+$setting = fetch_assoc("SELECT nama_instansi, alamat_instansi, kabupaten, kontak, email FROM setting LIMIT 1");
 ?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -20,8 +28,9 @@ $nama_user = $_SESSION['nama_pengaju']; // gunakan nama_pengaju sesuai login.php
   <header class="header">
     <div class="logo"><?php include '../assets/logo.php'; ?></div>
     <div class="instansi">
-      <h1>MENU PENGAJUAN</h1>
-      <p>Selamat datang, <strong><?= htmlspecialchars($nama_user) ?></strong></p>
+      <h1><?= $setting['nama_instansi'] ?></h1>
+      <p><?= $setting['alamat_instansi'] ?> – <?= $setting['kabupaten'] ?></p>
+      <p><?= $setting['kontak'] ?> | <?= $setting['email'] ?></p>
     </div>
     <div id="clock"></div>
     <div id="next-prayer"></div>
